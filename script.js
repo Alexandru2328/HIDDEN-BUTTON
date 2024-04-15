b37484852a3ccf74e9dc66cc540a432edcd85112
Alexandru2328 <Alexandru.romario07@gmail.com>
2024-04-15 09:07:16 +0100

    repair details

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 let btnRandom = 0;
 
-function getBtnRandom(nrOfButtons) {
+function setBtnRandom(nrOfButtons) {
     btnRandom = Math.floor(Math.random() * nrOfButtons) + 1;
 }
 
 function checkWinner(buttonId) {
     if (btnRandom === buttonId) {
-        document.getElementById("rezult").innerText = "CONGRATULATIONS!! YOU CHOOSE THE CORRECT BUTTON";
+        document.getElementById("result").innerText = 
+        "CONGRATULATIONS!! YOU CHOOSE THE CORRECT BUTTON";
     } else {
-        document.getElementById("rezult").innerText = "TRY AGAIN";
+        document.getElementById("result").innerText = "TRY AGAIN";
     }
 }
 
 function newGame() {
-    let element = document.querySelectorAll('#buttonsContainer, #rezult');
+    let element = document.querySelectorAll('#buttonsContainer, #result');
     let newGameBtn = document.getElementById ("newGame");
     element.forEach(function(element) {
         element.innerHTML = ' ';
-        if (newGameBtn){
+        if (newGameBtn) {
             const parentEl = document.getElementById("Game");
             parentEl.removeChild(newGameBtn);
         }
     });
 }
+
 function createNumberBtn() {
     let nrOfButtons = document.getElementById("nrOfBtn").value;
-    getBtnRandom(nrOfButtons);
+    setBtnRandom(nrOfButtons);
     let buttonsContainer = document.getElementById("buttonsContainer");
     for (let i = 1; i <= nrOfButtons; ++i) {
         (function(buttonId) {
             const button = document.createElement("button");
-            button.textContent = `Button`;
+            button.textContent = 'Button';
             button.addEventListener('click', function() {
                 checkWinner(buttonId);
             });
@@ -38,7 +40,7 @@ function createNumberBtn() {
         })(i);
     }
     const button = document.createElement("button");
-    button.textContent = `New Game`;
+    button.textContent = 'New Game';
     button.id = 'newGame';
     button.addEventListener('click', function() {
         newGame();
