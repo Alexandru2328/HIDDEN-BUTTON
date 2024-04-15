41d46cb74f05716d8c6d8a7ae55fb28577e852e0
Alexandru2328 <Alexandru.romario07@gmail.com>
2024-04-15 10:19:51 +0100

    repair bug

--- script.js
+++ script.js
@@ -15,29 +15,31 @@ function checkWinner(buttonId) {
 
 function newGame() {
     let element = document.querySelectorAll('#buttonsContainer, #result');
-    let newGameBtn = document.getElementById ("newGame");
-    element.forEach(function(element) {
-        element.innerHTML = ' ';
-        if (newGameBtn) {
-            const parentEl = document.getElementById("Game");
+    let newGameBtn = document.getElementById("newGame");
+    element.forEach(function(el) {
+        el.innerHTML = '';
+    });
+    if (newGameBtn) {
+        const parentEl = document.getElementById("Game");
+        if (parentEl.contains(newGameBtn)) {
             parentEl.removeChild(newGameBtn);
         }
-    });
+    }
 }
+
 
 function createNumberBtn() {
     let nrOfButtons = document.getElementById("nrOfBtn").value;
     setBtnRandom(nrOfButtons);
     let buttonsContainer = document.getElementById("buttonsContainer");
     for (let i = 1; i <= nrOfButtons; ++i) {
-        (function(buttonId) {
-            const button = document.createElement("button");
-            button.textContent = 'Button';
-            button.addEventListener('click', function() {
-                checkWinner(buttonId);
-            });
-            buttonsContainer.appendChild(button);
-        })(i);
+        const button = document.createElement("button");
+        button.textContent = 'Button';
+        button.param = i;
+        button.addEventListener('click', function() {
+            checkWinner(button.param);
+        });
+        buttonsContainer.appendChild(button);
     }
     const button = document.createElement("button");
     button.textContent = 'New Game';
